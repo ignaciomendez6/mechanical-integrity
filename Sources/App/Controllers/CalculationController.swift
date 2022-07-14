cce425996c0638b53b657df341c413fcbfc31ab6
IgnacioMendez <ignaciomendez6@gmail.com>
2022-07-14 18:53:03 -0300

    head ellipsoidal and new format

--- Sources/App/Controllers/CalculationController.swift
+++ Sources/App/Controllers/CalculationController.swift
@@ -11,41 +11,61 @@ import Foundation
 
 struct CalculationController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let calculationRoute = routes.grouped("calculations") // http://127.0.0.1:8080/calculations creo el grupo /users
         
-        // Create a route group that requires the SessionToken JWT.
+        // MARK: - create route /calculations
+        /// http://127.0.0.1:8080/calculations
+        let calculationRoute = routes.grouped("calculations")
+        
+        // MARK: - Create a route group that requires the SessionToken JWT.
         let secure = calculationRoute.grouped(SessionToken.authenticator(), SessionToken.guardMiddleware())
-        secure.post("shell", use: shell) // POST http://127.0.0.1:8080/calculations/shell POST acá calculo la envolvente
-        secure.get("getAllCalculation", ":userId", use: getAllCalcultations) // GET http://127.0.0.1:8080/calculations/getAllCalculation/dasdsadasd654564654 me traigo todos los calculos de ese userId
-        secure.get("getOneCalculation", ":calculationId", use: getOneCalculation) // GET http://127.0.0.1:8080/calculations/getOneCalculation/dasdsadasd654564654 me traigo un solo usuario
+        
+        // MARK: - POST shell is calculated (header JWT)
+        /// http://127.0.0.1:8080/calculations/shell
+        secure.post("shell", use: shell)
+        
+        // MARK: - POST headEllipsoidal is calculated (header JWT)
+        /// http://127.0.0.1:8080/calculations/headEllipsoidal
+        secure.post("headEllipsoidal", use: headellipsoidal)
+        
+        // MARK: - GET get all calculation for a userId
+        /// http://127.0.0.1:8080/calculations/getAllCalculation/08fd9e97-4565-49f2-ad30-cf59ecda83b9
+        secure.get("getAllCalculation", ":userId", use: getAllCalcultations)
+        
+        // MARK: - GET get one calculation
+        /// http://127.0.0.1:8080/calculations/getOneCalculation/02f2042a-db22-49a0-810d-165c6287950b
+        secure.get("getOneCalculation", ":calculationId", use: getOneCalculation)
     }
     
     func shell(req: Request) async throws -> Calculation {
         print("shell")
-        
         let calculation = try req.content.decode(Calculation.self)
-        
         let shell = Shell(P: calculation.P, D: calculation.D, S: calculation.S, E: calculation.E)
         let trShell = shell.trShell()
-        
         calculation.tr = trShell
         calculation.date = Date()
         calculation.component = "shell"
-        
         try await calculation.save(on: req.db)
-        
+        return calculation
+    }
+    
+    func headellipsoidal(req: Request) async throws -> Calculation {
+        print("headellipsoidal")
+        let calculation = try req.content.decode(Calculation.self)
+        let headEllipsoidal = HeadEllipsoidal(P: calculation.P, D: calculation.D, S: calculation.S, E: calculation.E)
+        let trHeadEllipsoidal = headEllipsoidal.trHeadEllipsoidal()
+        calculation.tr = trHeadEllipsoidal
+        calculation.date = Date()
+        calculation.component = "Head Ellipsoidal"
+        try await calculation.save(on: req.db)
         return calculation
     }
     
     func getAllCalcultations(req: Request) async throws -> [Calculation] {
         print("getAllcalculation")
-        
         let id = req.parameters.get("userId") ?? ""
         let uuid = UUID(uuidString: id) ?? UUID()
-        
         let calculations = try await Calculation.query(on: req.db).filter(\.$user.$id == uuid).all()
-        
-        return calculations // no devuelvo el password al cliente
+        return calculations
     }
     
     func getOneCalculation(req: Request) async throws -> Calculation {
@@ -53,7 +73,6 @@ struct CalculationController: RouteCollection {
         guard let calculation = try await  Calculation.find(req.parameters.get("calculationId"), on: req.db) else {
             throw Abort(.notFound)
         }
-        
         return calculation
     }
 }
--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -13,44 +13,64 @@ import SendGrid
 
 struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let usersRoute = routes.grouped("users") // http://127.0.0.1:8080/users creo el grupo /users
-        usersRoute.post(use: createUser) // http://127.0.0.1:8080/users POST acá creo el usuario
         
-        usersRoute.put("checkemail", "code", ":code","id", ":id", use: checkEmail) // PUT http://127.0.0.1:8080/users/checkemail/code/YvI3ZNMx/id/fbdb54b9-b070-4bd8-a47b-e3d75bb09d8c PUT acá verifico el email al hacer sign up, envío un código al email
+        // MARK: - create route /users
+        /// http://127.0.0.1:8080/users
+        let usersRoute = routes.grouped("users")
         
-        usersRoute.put("resetpassword", "email", ":email", use: resetPassword) // PUT http://127.0.0.1:8080/users/resetpassword/email/4@gmail.com PUT acá envío un nuevo código al email para resetar el password
+        // MARK: - POST create user if dosn't exist and send check email
+        /// http://127.0.0.1:8080/users
+        usersRoute.post(use: createUser)
         
-        usersRoute.put("resetpasswordcheckemail", "code", ":code", "id", ":id", "password", ":password", use: resetPasswordCheckEmail) // PUT http://127.0.0.1:8080/users/resetpasswordcheckemail/code/L6oqyobE/id/fbdb54b9-b070-4bd8-a47b-e3d75bb09d8c/password/44 aca verifico el mail y cambio el password
+        // MARK: - PUT check email in sign up
+        /// http://127.0.0.1:8080/users/checkemail/code/GtsEQ8/id/7e24f183-ec9e-4e50-b9d1-c31c311cca4a
+        usersRoute.put("checkemail", "code", ":code","id", ":id", use: checkEmail)
         
-        // Creo el token
+        // MARK: - PUT send new code to email to reset password
+        /// http://127.0.0.1:8080/users/resetpassword/email/4@gmail.com
+        usersRoute.put("resetpassword", "email", ":email", use: resetPassword)
+        
+        // MARK: - PUT check mail and change password
+        /// http://127.0.0.1:8080/users/resetpasswordcheckemail/code/vFADAN/id/7e24f183-ec9e-4e50-b9d1-c31c311cca4a/password/44
+        usersRoute.put("resetpasswordcheckemail", "code", ":code", "id", ":id", "password", ":password", use: resetPasswordCheckEmail)
+        
+        // MARK: - Create token
         let passwordProtected = usersRoute.grouped(User.authenticator(), User.guardMiddleware())
-        passwordProtected.post("login", use: login) // POST http://127.0.0.1:8080/users/login acá envío en headear usuario y password, si está ok devuelve en JWT
         
-        // Create a route group that requires the SessionToken JWT.
+        // MARK: - POST send headear mail and password in basic auth ok? -> JWT
+        /// http://127.0.0.1:8080/users/login
+        passwordProtected.post("login", use: login)
+        
+        // MARK: - Create a route group that requires the SessionToken JWT.
         let secure = usersRoute.grouped(SessionToken.authenticator(), SessionToken.guardMiddleware())
-        secure.get(":userId", use: getOneUser) // GET http://127.0.0.1:8080/users/323A49A2-3751-4BD8-AFBE-58F8CF26A0D4 obtengo un usuarios, en header el JWT
-        secure.get(use: getAllusers) // GET http://127.0.0.1:8080/users obtengo todos los usuarion, en header el JWT
-        secure.delete(":userId", use: deleteUser) // DELETE http://127.0.0.1:8080/users/bf6844b8-5a71-4398-a9af-8556b3555f08 elimino un usuario, en header el JWT
+        
+        // MARK: - GET get one user (header JWT)
+        /// http://127.0.0.1:8080/users/323A49A2-3751-4BD8-AFBE-58F8CF26A0D4
+        secure.get(":userId", use: getOneUser)
+        
+        // MARK: - GET get all users (header JWT)
+        /// http://127.0.0.1:8080/users
+        secure.get(use: getAllusers)
+        
+        // MARK: - DELETE deleted user with id, en header el JWT
+        /// http://127.0.0.1:8080/users/bf6844b8-5a71-4398-a9af-8556b3555f08
+        secure.delete(":userId", use: deleteUser)
     }
     
-    // http://127.0.0.1:8080/users POST: aca se crea un usuario nuevo si este no existe y se envía email con código para verificar
     func createUser(req: Request) async throws -> UserPublic {
         print("createUser")
         let user = try req.content.decode(User.self)
-        user.password = try Bcrypt.hash(user.password) //encripto la contrasena
-        user.codeEmail = [UInt8].random(count: 6).base64 // creo el código para verificar email
+        user.password = try Bcrypt.hash(user.password) //crypt password
+        user.codeEmail = String.random(length: 6) // code verification create
         user.checked = false
         user.date = Date() + 5*60
         try await user.save(on: req.db)
-        
-        let resultEmail = try sendEmail(req: req, code: user.codeEmail ?? "", email: user.email) // envío mail con código utilizando el endpoint
+        let resultEmail = try sendEmail(req: req, code: user.codeEmail ?? "", email: user.email) // send email with code
         print(resultEmail)
-        
         let userPublic = UserPublic(id: user.id, email: user.email)
         return userPublic
     }
     
-    // http://127.0.0.1:8080/users/checkemail/code/9689/id/e313421d-6f0f-48a7-86e5-e5410c1bd209 PUT: "checkemail", "code", ":code","id", ":id", se verifica el email
     func checkEmail(req: Request) async throws -> HTTPStatus {
         print("checkemail")
         guard let codeParam = req.parameters.get("code")
@@ -70,7 +90,6 @@ struct UserController: RouteCollection {
         return .ok
     }
     
-    // http://127.0.0.1:8080/users/login POST: aca se crea el token, mandar en el header Basic auth el email y contrasena
     func login(req: Request) async throws -> ClientTokenReponse {
         print("login")
         let user = try req.auth.require(User.self)
@@ -82,8 +101,7 @@ struct UserController: RouteCollection {
         }
     }
     
-    // http://127.0.0.1:8080/users/resetpassword/email/ignaciomendezing@gmail.com PUT: Aca reseteo el codigo de verificacion y envio mail al cliente usersRoute.put("resetpassword", "email", ":email", use: resetPassword)
-    func resetPassword(req: Request) async throws -> HTTPStatus {
+    func resetPassword(req: Request) async throws -> UserPublic {
         guard let email = req.parameters.get("email") else {
             throw Abort(.badRequest)
         }
@@ -91,16 +109,14 @@ struct UserController: RouteCollection {
         guard let userDb = try await User.find(users?.id, on: req.db) else {
             throw Abort(.badRequest)
         }
-        //        userDb.checked = false
         userDb.date = Date() + 5*60
-        userDb.codeEmail = [UInt8].random(count: 6).base64 // creo el código para verificar email // creo el código para verificar email
+        userDb.codeEmail = String.random(length: 6) // create code to check email
         let resultEmail = try sendEmail(req: req, code: userDb.codeEmail ?? "", email: userDb.email)
         print(resultEmail)
         try await userDb.update(on: req.db)
-        return .ok
+        return UserPublic(id: users?.id, email: email)
     }
     
-    // http://127.0.0.1:8080/users/resetpasswordcheckemail/code/9689/id/e313421d-6f0f-48a7-86e5-e5410c1bd209/password/1234 PUT verificio el código enviado al mail y reseto el password
     func resetPasswordCheckEmail(req: Request) async throws -> HTTPStatus {
         print("checkemail")
         guard let codeParam = req.parameters.get("code"),
@@ -113,7 +129,7 @@ struct UserController: RouteCollection {
         }
         if (userDb.codeEmail == codeParam) && (userDb.date ?? Date() > Date()) {
             userDb.checked = true
-            userDb.password = try Bcrypt.hash(password) //encripto la contrasena
+            userDb.password = try Bcrypt.hash(password) // crypt password
             
             try await userDb.update(on: req.db)
             
@@ -123,22 +139,22 @@ struct UserController: RouteCollection {
         return .ok
     }
     
-    // está funcion en un reedirecionamiento de createUser para enviar el mail
+    // Redirect email
     func sendEmail(req: Request, code: String, email: String) throws -> EventLoopFuture<HTTPStatus> {
         print("sendEmail")
         var emailContent: [String: String] = [:]
         emailContent["type"] = "text/html"
-        emailContent["value"] = "Codigo de verificación \(code)"//mensaje del email
-        let email = SendGridEmail(personalizations: [Personalization(to: [EmailAddress(email: email, name: email)],//destinatario
+        emailContent["value"] = "Codigo de verificación \(code)"//message email
+        let email = SendGridEmail(personalizations: [Personalization(to: [EmailAddress(email: email, name: email)], // receiver
                                                                      cc: nil,
                                                                      bcc: nil,
-                                                                     subject: "Verificacion de código",//asunto del mail
+                                                                     subject: "Verificacion de código", // email subject
                                                                      headers: nil,
                                                                      substitutions: nil,
                                                                      dynamicTemplateData: nil,
                                                                      customArgs: nil,
                                                                      sendAt: nil)],
-                                  from: EmailAddress(email: "ignaciomendez6@gmail.com", name: "Mechanical Integrity App"),// quien envía el mail
+                                  from: EmailAddress(email: "ignaciomendez6@gmail.com", name: "Mechanical Integrity App"), // sender
                                   replyTo: nil,
                                   subject: nil,
                                   content: [emailContent],
@@ -162,7 +178,6 @@ struct UserController: RouteCollection {
         }
     }
     
-    // http://127.0.0.1:8080/users GET: aca muestro todos los usarios
     func getAllusers(req: Request) async throws -> [UserPublic] {
         print("getAllusers")
         let users = try await User.query(on: req.db).all()
@@ -170,10 +185,9 @@ struct UserController: RouteCollection {
         for i in 0..<users.count {
             usersPublic.append(UserPublic(id: users[i].id, email: users[i].email))
         }
-        return usersPublic // no devuelvo el password al cliente
+        return usersPublic // no return password
     }
     
-    // http://127.0.0.1:8080/users:userId GET: aca muestro un usuario
     func getOneUser(req: Request) async throws -> UserPublic {
         print("getOneUser")
         guard let user = try await  User.find(req.parameters.get("userId"), on: req.db) else {
@@ -183,7 +197,6 @@ struct UserController: RouteCollection {
         return userPublic
     }
     
-    // http://127.0.0.1:8080/users:userId Delete: aca elimino un usuario segun su id
     func deleteUser(req: Request) async throws -> HTTPStatus {
         print("deleteUser")
         guard let user = try await User.find(req.parameters.get("userId"), on: req.db) else {
