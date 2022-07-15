7538adda8a953c51c4b28ba9bacf0ca618fb8175
IgnacioMendez <ignaciomendez6@gmail.com>
2022-07-15 18:33:02 -0300

    enviroment var for postgress

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,13 +10,19 @@ public func configure(_ app: Application) throws {
 //    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 //    app.middleware.use(app.sessions.middleware)
     
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "mi_database"
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration?.certificateVerification = .none
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? "mi_database"
+        ), as: .psql)
+    }
     
     app.migrations.add(CreateUser())
     app.migrations.add(CreateCalculation())
